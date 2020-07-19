2b831849dd1cd17dd5a7f80f5e638c36a39695a2
Isaac Medina <isaac@Isaacs-MacBook-Air.local>
2020-07-19 12:48:33 -0500

    added styling

---
+++ components/MealItem.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+
+const MealItem = (props) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={props.onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground
+              source={{ uri: props.imageUrl }}
+              style={styles.bgImage}
+            >
+              <View style={styles.titleContainer}>
+                <Text numberOfLines={1} style={styles.title}>
+                  {props.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetails }}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity}</Text>
+            <Text>{props.affordability}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  mealRow: {
+    flexDirection: "row",
+  },
+  mealHeader: {
+    height: "85%",
+  },
+  bgImage: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "open-sans-bold",
+    color: "white",
+    textAlign: "center",
+  },
+  mealDetails: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+});
+
+export default MealItem;
---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,30 @@
+import { Platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailsScreen from "../screens/MealDetailsScreen";
+
+import colors from "../constants/colors";
+
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen,
+    },
+    MealDetails: MealDetailsScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(MealsNavigator);
